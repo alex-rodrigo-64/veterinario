cd7572d2c4dc146b226dc11dea87241a5cabcf0c
alex-rodrigo-64 <alexcalle64@gmail.com>
2022-01-28 00:57:55 -0400

    menu

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,63 +2,67 @@
 
 @section('content')
 
+<div class="row justify-content-center">
 <div class="row">
     <div class="col-lg-6">
       <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-
-          <p class="card-text">
-            Some quick example text to build on the card title and make up the bulk of the cards
-            content.
-          </p>
-
-          <a href="#" class="card-link">Card link</a>
-          <a href="#" class="card-link">Another link</a>
+        <div class="card-body text-center">
+            <a  href="http://www.stackoverflow.com/">
+                <button class="btn btn-primary btn-lg" style='width:300px; height:100px; background: linear-gradient(100deg, #f7f9fd, rgb(27, 255, 65));'>
+                    <b class="text-dark">
+                        <FONT SIZE=6>R</font><FONT SIZE=6>E</font><FONT SIZE=6>C</font><FONT SIZE=6>E</font><FONT SIZE=6>P</font><FONT SIZE=6>C</font><FONT SIZE=6>I</font><FONT SIZE=6>O</font><FONT SIZE=6>N</FONT>
+                    </b>    
+                </button>
+            </a>
         </div>
       </div>
-
-      <div class="card card-primary card-outline">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-
-          <p class="card-text">
-            Some quick example text to build on the card title and make up the bulk of the cards
-            content.
-          </p>
-          <a href="#" class="card-link">Card link</a>
-          <a href="#" class="card-link">Another link</a>
+        
+      <div class="card">
+        <div class="card-body text-center">
+            <a  href="http://www.stackoverflow.com/">
+                <button class="btn btn-primary btn-lg" style='width:300px; height:100px; background: linear-gradient(100deg, #f7f9fd, rgb(27, 106, 255));'>
+                    <b class="text-dark">
+                        <FONT SIZE=6>S</font><FONT SIZE=6>E</font><FONT SIZE=6>R</font><FONT SIZE=6>V</font><FONT SIZE=6>I</font><FONT SIZE=6>C</font><FONT SIZE=6>I</font><FONT SIZE=6>O</font><FONT SIZE=6>S</FONT>
+                    </b>    
+                </button>
+            </a>
         </div>
-      </div><!-- /.card -->
+      </div>
     </div>
     <!-- /.col-md-6 -->
     <div class="col-lg-6">
-      <div class="card">
-        <div class="card-header">
-          <h5 class="m-0">Featured</h5>
-        </div>
-        <div class="card-body">
-          <h6 class="card-title">Special title treatment</h6>
+        <div class="card">
+            <div class="card-body text-center">
+                <a  href="http://www.stackoverflow.com/">
+                    <button class="btn btn-primary btn-lg" style='width:300px; height:100px; background: linear-gradient(100deg, #f7f9fd, rgb(226, 255, 62));'>
+                        <b class="text-dark">
+                            <FONT SIZE=6>V</font><FONT SIZE=6>E</font><FONT SIZE=6>N</font><FONT SIZE=6>T</font><FONT SIZE=6>A</font><FONT SIZE=6>S</font>
+                            
+                        </b>    
+                    </button>
+                </a>
+            </div>
+          </div>
 
-          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-
-      <div class="card card-primary card-outline">
-        <div class="card-header">
-          <h5 class="m-0">Featured</h5>
-        </div>
-        <div class="card-body">
-          <h6 class="card-title">Special title treatment</h6>
-
-          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
+          <div class="card">
+            <div class="card-body text-center">
+                <a  href="http://www.stackoverflow.com/">
+                    <button class="btn btn-primary btn-lg" style='width:300px; height:100px; background: linear-gradient(100deg, #f7f9fd, rgb(255, 155, 62));'>
+                        <b class="text-dark">
+                            <FONT SIZE=6>H</font><FONT SIZE=6>I</font><FONT SIZE=6>S</font><FONT SIZE=6>T</font><FONT SIZE=6>O</font><FONT SIZE=6>R</font><FONT SIZE=6>I</font><FONT SIZE=6>A</font><FONT SIZE=6>L</FONT>
+                        </b>    
+                    </button>
+                </a>
+            </div>
+          </div>
     </div>
     <!-- /.col-md-6 -->
   </div>
   <!-- /.row -->
+</div>
+
+
+
+  
 
 @endsection
