3648dbeb8540dc08b5e92298c0552df3a223f74d
alex-rodrigo-64 <alexcalle64@gmail.com>
2022-01-28 01:58:29 -0400

    login

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware('auth');;
 
-Route::get('admin', function () {
-    return view('admin.dashboard');
-});
 
+Auth::routes();
+
+//Route::get('/home', 'HomeController@index')->name('home');
